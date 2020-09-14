f7aa3f2ca23f7b69f5d7c317dcf508009e3faaa6
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2020-09-13 19:18:34 -0700

    Add test for coordsToPolygon method

---
+++ polygon_test.go
@@ -0,0 +1,84 @@
+package mfcg
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func Test_coordsToPolygon(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Polygon
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Single slice",
+			args: args{
+				data: []byte(`[[[11.1, 11.1], [22.2, 22.2], [33.3, 33.3]]]`),
+			},
+			want:    &Polygon{Coords: [][]Point{{{X: 11.1, Y: 11.1}, {X: 22.2, Y: 22.2}, {X: 33.3, Y: 33.3}}}},
+			wantErr: false,
+		},
+		{
+			name: "Multiple slices",
+			args: args{
+				data: []byte(`[
+					[[11.1, 11.1]],
+					[[22.2, 22.2]],
+					[[33.3, 33.3]]
+					]`),
+			},
+			want: &Polygon{Coords: [][]Point{
+				{{X: 11.1, Y: 11.1}},
+				{{X: 22.2, Y: 22.2}},
+				{{X: 33.3, Y: 33.3}},
+			}},
+		},
+		{
+			name: "Zero slices",
+			args: args{
+				data: []byte(`[]`),
+			},
+			want: &Polygon{
+				Coords: [][]Point{},
+			},
+			wantErr: false,
+		},
+		{
+			name: "No data",
+			args: args{
+				data: []byte(``),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Invalid data type",
+			args: args{
+				data: []byte(`[[["foo", "bar"]]]`),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			got, err := coordsToPolygon(test.args.data)
+			if (err != nil) != test.wantErr {
+				t.Errorf("got: <%v>, want error: <%v>", err, test.wantErr)
+				return
+			}
+
+			if diff := cmp.Diff(got, test.want); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
