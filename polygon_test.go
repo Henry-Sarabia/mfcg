7991ea690f873add65154b6ca0c0d6963d8874e2
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2020-09-15 18:39:32 -0700

    Add tests for geosToPolygons method

--- polygon_test.go
+++ polygon_test.go
@@ -152,3 +152,72 @@ func Test_coordsToPolygons(t *testing.T) {
 		})
 	}
 }
+
+func Test_geosToPolygons(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []Polygon
+		wantErr bool
+	}{
+		{
+			name: "Single geometry",
+			args: args{
+				[]byte(`[{"width": 11.1, "coordinates": [[[22.2, 22.2], [33.3, 33.3], [44.4, 44.4]]]}]`),
+			},
+			want:    []Polygon{{Width: 11.1, Coords: [][]Point{{{22.2, 22.2}, {33.3, 33.3}, {44.4, 44.4}}}}},
+			wantErr: false,
+		},
+		{
+			name: "Multiple geometries",
+			args: args{
+				[]byte(`[
+					{"width": 11.1, "coordinates": [[[11.1, 11.1]]]},
+					{"width": 22.2, "coordinates": [[[22.2, 22.2]]]},
+					{"width": 33.3, "coordinates": [[[33.3, 33.3]]]}
+				]`),
+			},
+			want: []Polygon{
+				{Width: 11.1, Coords: [][]Point{{{X: 11.1, Y: 11.1}}}},
+				{Width: 22.2, Coords: [][]Point{{{X: 22.2, Y: 22.2}}}},
+				{Width: 33.3, Coords: [][]Point{{{X: 33.3, Y: 33.3}}}},
+			},
+			wantErr: false,
+		},
+		{
+			name:    "Zero geometries",
+			args:    args{[]byte(`[]`)},
+			want:    nil,
+			wantErr: false,
+		},
+		{
+			name:    "No data",
+			args:    args{[]byte(``)},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "Invalid data type",
+			args:    args{[]byte(`[{"width": "foobar"}]`)},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			got, err := geosToPolygons(test.args.data)
+			if (err != nil) != test.wantErr {
+				t.Errorf("got: <%v>, want error: <%v>", err, test.wantErr)
+				return
+			}
+
+			if diff := cmp.Diff(got, test.want); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
